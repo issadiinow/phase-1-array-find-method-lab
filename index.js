c7ad83e776745e680525359930e4334119dd3e57
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2024-12-19 22:59:49 +0300

    completed array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use the find() method to locate the first win (where result is 'W')
+    const winRecord = record.find(game => game.result === "W");
+  
+    // If a win is found, return the year of the win; otherwise, return undefined
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
